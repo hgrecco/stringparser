010d6aedf0e72304b4b89bc2f7f8554c863bdbeb
Hernan Grecco <hernan.grecco@gmail.com>
2011-10-28 22:19:16 +0200

    Simple and compound field names now supported.

--- stringparser.py
+++ stringparser.py
@@ -17,7 +17,7 @@
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 # OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# DummyS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
@@ -31,7 +31,6 @@ information within strings. As patterns are given using the familiar format
 string specification (PEP 3101), writing them is much easier than writing 
 regular expressions (albeit less powerful).
 
-Check out the Parser docstring for examples.
 
 Examples
 --------
@@ -69,12 +68,13 @@ You can ignore some fields using _ as a name::
     >>> Parser('The {_:s} is {:d}')('The answer is 42')
     42
 
+
 Limitations
 -----------
 
 - From the format string:
   `[[fill]align][sign][#][0][minimumwidth][.precision][type]`
-  only type, sign and # are currently implemented.
+  only `type`, `sign` and `#` are currently implemented.
   This might cause trouble to match certain notation like:
 
   - decimal: '-4' written as '-     4'
@@ -93,6 +93,7 @@ __version__ = '0.2'
 
 import re
 import sys
+import copy
 import string
 if sys.version_info[0] < 3:
     from StringIO import StringIO
@@ -102,6 +103,13 @@ else:
 from functools import partial
 
 from collections import OrderedDict
+
+
+class Dummy():
+    """
+    """
+    pass
+
 
 _FORMATTER = string.Formatter()
 
@@ -128,7 +136,6 @@ _REG = {None: ('.*?', str),
 #
 #    [[fill]align][sign][#][0][width][,][.precision][type]
 #
-#    format_spec ::=  [[fill]align][sign][#][0][width][,][.precision][type]
 #    fill        ::=  <a character other than '}'>
 #    align       ::=  "<" | ">" | "=" | "^"
 #    sign        ::=  "+" | "-" | " "
@@ -142,8 +149,15 @@ _FMT = re.compile("(?P<align>(?P<fill>[^{}])?[<>=\^])?"
 
 
 def fmt_to_regex(fmt):
-    """ Returns the regex to match and the callable to convert from string  
-    or a standard format specifier string.
+    """ For a given standard format specifier string it returns
+    with the regex to match and the callable to convert from string.
+
+    Not implemented: fill, align, width precision
+
+    :param fmt: format specifier string as defined in :pep:3101
+    :type fmt: string
+    :return: (regex, converter)
+    :rtype: tuple
     """
 
     (align, fill, sign, alternate, 
@@ -172,6 +186,147 @@ def fmt_to_regex(fmt):
     return reg, fun
 
 
+def split_field_name(name):
+    """Split a compound field name into multiple simple field names.
+
+    :param name: simple or compound field name
+    """
+
+    first = True
+    for namepart in name.split('.'):
+        # Split that part by open bracket chars
+        keyparts = namepart.split('[')
+        # The first part is just a bare name
+        key = keyparts[0]
+
+        # Empty strings are not allowed as field names
+        if key == '':
+            raise ValueError("empty field name in {}".format(name))
+
+        # The first name in the sequence is used to index
+        # the args/kwargs arrays. Subsequent names are used
+        # on the result of the previous operation.
+        if first:
+            first = False
+            yield ('item', key)
+        else:
+            yield ('attribute', key)
+
+        # Now process any bracket expressions which followed
+        # the first part.
+        for key in keyparts[1:]:
+            endbracket = key.find(']')
+            if endbracket < 0 or endbracket != len(key) - 1:
+                raise ValueError("Invalid field syntax in {}".format(name))
+
+            # Strip off the closing bracket and try to coerce to int
+            key = key[:-1]
+            try:
+                key = int(key)
+            except ValueError:
+                pass
+
+            yield ('item', key)
+
+
+def build_hierarchy(field_parts, top):
+    """Build a hierarchy of dictionary and Dummy object
+
+    :param field_parts: iterable of simple field names and type
+    :param top: element to be placed on the top of the hierarchy
+    """
+    for typ, name in reversed(list(field_parts)):
+        if typ == 'attribute':
+            tmp = Dummy()
+            setattr(tmp, name, top)
+            top = tmp
+        elif typ == 'item':
+            tmp = dict()
+            tmp[name] = top
+            top = tmp
+    return top        
+
+
+def append_to_hierarchy(bottom, field_parts, top):
+    """Append hierarchy to another.
+
+    :param bottom: existing hierarchy
+    :param field_parts: iterable of simple field names and type
+    :param top: element to be placed on the top of the hierarchy
+    """
+    for typ_, name in field_parts:
+        if isinstance(bottom, dict):
+            if not typ_ == 'item':
+                raise ValueError("Incompatible {}, {}".format(typ_, name))
+            try:
+                bottom = bottom[name]
+            except KeyError:
+                bottom[name] = build_hierarchy(field_parts, top)
+                
+        elif isinstance(bottom, Dummy):
+            if not typ_ == 'attribute':
+                raise ValueError("Incompatible {}, {}".format(typ_, name))
+            try:
+                bottom = getattr(bottom, name)
+            except AttributeError:
+                setattr(bottom, name, 
+                        build_hierarchy(field_parts, top))
+        else:
+             raise ValueError("Incompatible {}, {}".format(typ_, name))
+
+
+def set_in_hierarchy(bottom, field_parts, top):
+    """Traverse a hierarchy and set the top element.
+
+    :param bottom: existing hierarchy
+    :param field_parts: iterable of simple field names and type
+    :param top: element to be placed on the top of the hierarchy
+    """
+    for typ_, name in field_parts:
+        if isinstance(bottom, dict):
+            if bottom[name] is None:
+                bottom[name] = top
+            else:
+                set_in_hierarchy(bottom[name], field_parts, top)
+        elif isinstance(bottom, Dummy):
+            if getattr(bottom, name) is None:
+                setattr(bottom, name, top)
+            else:
+                set_in_hierarchy(getattr(bottom, name), field_parts, top)
+        elif isinstance(bottom, list):
+            if bottom[int(name)] is None:
+                bottom[int(name)] = top
+            else:
+                set_in_hierarchy(bottom[int(name)], field_parts, top)
+
+
+def convert(obj):
+    """Recursively traverse template data structure converting dictionaries
+    to lists if all keys are numbers which fill the range from [0, len(keys))
+
+    :param obj: nested template data structure
+    """
+    if obj is None:
+        return obj
+
+    elif isinstance(obj, dict):
+        try:
+            keys = [int(key) for key in obj.keys()]
+            if min(keys) == 0 and max(keys) == len(keys) -1:
+                return [convert(obj[str(key)]) for key in keys]
+        except:
+            pass
+
+        for key, value in obj.items():
+            obj[key] = convert(value)
+        return obj
+
+    elif isinstance(obj, Dummy):
+        for key, value in obj.__dict__.items():
+            setattr(obj, key, convert(value))
+        return obj
+
+
 class Parser(object):
     """Callable object to parse a text line using a format string (PEP 3101) 
     as a template.
@@ -187,10 +342,12 @@ class Parser(object):
         self._output_dict = False
 
         pattern = StringIO()
-        max_numeric = 0
+        number = 0
 
-        # Assembly regex, list of fields and converter function by inspecting
-        # each replacement field.
+        # Assembly regex, list of fields, converter function,
+        # and output template data structure by inspecting
+        # each replacement field. 
+        template = OrderedDict()
         for literal, field, fmt, conv in _FORMATTER.parse(format_string):
             pattern.write(re.escape(literal))
 
@@ -207,19 +364,16 @@ class Parser(object):
             if field == '_':
                 pattern.write('(?:' + reg + ')')
                 continue
-            elif field == '':
-                max_numeric += 1
-                field = max_numeric
-            else:
-                try:
-                    field = int(field)
-                    max_numeric = field
-                except ValueError:
-                    self._output_dict = True
-            
+
+            if not field or field[0] in ('.', '['):
+                field = str(number) + field
+                number += 1
+
             pattern.write('(' + reg + ')')
             self._fields.append((field, fun))
+            append_to_hierarchy(template, split_field_name(field), None)
         
+        self._template = convert(template)
         self._regex = re.compile('^' + pattern.getvalue() + '$')
 
     def __call__(self, text):
@@ -230,30 +384,17 @@ class Parser(object):
             raise ValueError("Could not parse "
                              "'{}' with '{}'".format(text, self._regex.pattern))
 
-        # Build a dictionary mapping the field name to the corresponding value
-        # converted from string.
-        parsed = OrderedDict()
+        # Put each matched string in the corresponding output slot in the template
+        parsed = copy.deepcopy(self._template)
         for group, (field, fun) in zip(mobj.groups(), self._fields):
-            if isinstance(field, str) and '.' in field:
-                name, prop = field.split('.')
-                if not name in parsed:
-                    parsed[name] = OrderedDict()
-                parsed[field][prop] = fun(group)
-            else:
-                parsed[field] = fun(group)
-        
-        # If during parsing, any of the fields was found to have a non-numeric 
-        # name, return directly 
-        if self._output_dict:
-            return parsed
+            set_in_hierarchy(parsed, split_field_name(field), fun(group))
 
-        # If the result contains a single object, return it without Container 
-        if len(parsed) == 1:
-            return tuple(parsed.values())[0]
-        
-        # For the rest (multiple fields either unnamed or numbered), 
-        # return a tuple 
-        return tuple(parsed[key] for key in sorted(parsed.keys()))
+
+        # If the result is a list with a single object, return it without Container
+        if isinstance(parsed, list) and len(parsed) == 1:
+            return parsed[0]
+
+        return parsed
 
 
 if __name__ == '__main__':
--- test_parser.py
+++ test_parser.py
@@ -4,6 +4,10 @@ from collections import OrderedDict
 
 from stringparser import Parser
 
+class Dummy():
+    pass
+
+
 class ParserTest(unittest.TestCase):
 
     def _test(self, fstring, value):
@@ -22,19 +26,74 @@ class ParserTest(unittest.TestCase):
         text = fstring.format(**value)
         self.assertEqual(parser(text), OrderedDict(value))
 
-    def test_single_string(self):
+    def test_string(self):
         "Parse single string"
         self._test('before {0:s} after', 'TEST')
 
-    def test_escape_re_characters(self):
-        "Parse single string"
-        self._test('before * | {0:s} [ ( * .after', 'TEST')
+    @unittest.expectedFailure
+    def test_string_failure(self):
+        "Unimplemented features for string"
+        self._test("{0:x<7s}", "result")
+        self._test("{0:x<8s}", "result")
+        self._test("{0: <7s}", "result")
+        self._test("{0:<7s}", "result")
+        self._test("{0:>7s}", "result")
+        self._test("{0:>8s}", "result")
+        self._test("{0:=8s}", "result")
+        self._test("{0:^8s}", "result")
+        self._test("{0:^9s}", "result")
+        self._test("{0:^10s}", "result")
 
-    def test_single_int(self):
+    def test_escape_re_characters(self):
+        "Escape regex related characters"
+        self._test('start * | {0:s} [ ( * .after', 'TEST')
+
+    def test_int(self):
         "Parse single int"
         self._test('before {0:d} after', 42)
 
-    def test_single_binary(self):
+    @unittest.expectedFailure
+    def test_int_failure(self):
+        "Unimplemented features for int"
+        self._test("{0:()d}", -123)
+
+        self._test("{0:1000d}", 100)
+
+        self._test("{0:<d}", 1)
+        self._test("{0:>d}", 1)
+        self._test("{0:=d}", 1)
+        self._test("{0:^d}", 1)
+        self._test("{0:<d}", -1)
+        self._test("{0:>d}", -1)
+        self._test("{0:=d}", -1)
+        self._test("{0:^d}", -1)
+
+        self._test("{0:<10d}", 0)
+        self._test("{0:<10d}", 123)
+        self._test("{0:<10d}", -123)
+        self._test("{0:>10d}", 123)
+        self._test("{0:>10d}", -123)
+        self._test("{0:^10d}", 123)
+        self._test("{0:^10d}", -123)
+        self._test("{0:=10d}", 123)
+        self._test("{0:=+10d}", 123)
+        self._test("{0:=10d}", -123)
+        self._test("{0:=+10d}", -123)
+        self._test("{0:=()10d}", 123)
+
+        self._test("{0:=()10d}", -123)
+        self._test("{0:>()10d}", -123)
+        self._test("{0:<()10d}", -123)
+        self._test("{0:^()10d}", -123)
+
+        self._test("{0:d}", 10**100)
+        self._test("{0:d}", -10**100)
+        self._test("{0:+d}", 10**100)
+        self._test("{0:()d}", -10**100)
+        self._test("{0:()110d}", -10**100)
+        self._test("{0:()110d}", -10**100)
+
+    def test_binary(self):
         "Parse single binary"
         self._test('before {0:b} after', 42)
         self._test("{0:b}", 0)
@@ -55,12 +114,14 @@ class ParserTest(unittest.TestCase):
         self._test("{0:()b}", -(2**100 - 1))
         self._test("{0:=()200b}", -(2**100 - 1))
 
-    def test_single_octal(self):
+    def test_octal(self):
         "Parse single octal"
         self._test('before {0:o} after', 42)
         self._test('before {0:#o} after', 42)
+        self._test('before {0:o} after', -42)
+        self._test('before {0:#o} after', -42)
 
-    def test_single_hex(self):
+    def test_hex(self):
         "Parse single hex (lower and uppercase)"
         self._test('before {0:x} after', 42)
         self._test('before {0:X} after', 42)
@@ -68,17 +129,23 @@ class ParserTest(unittest.TestCase):
         self._test('before {0:#x} after', 42)
         self._test('before {0:#X} after', 42)
 
-    def test_single_fp_exp(self):
+        self._test('before {0:x} after', -42)
+        self._test('before {0:X} after', -42)
+
+        self._test('before {0:#x} after', -42)
+        self._test('before {0:#X} after', -42)
+
+    def test_fp_exp(self):
         "Parse single floating point exponential format (lower and uppercase)"
         self._test('before {0:e} after', 42.123e-10)
         self._test('before {0:E} after', 42.123e-10)
 
-    def test_single_fp_decimal(self):
+    def test_fp_decimal(self):
         "Parse single Floating point decimal format."
         self._test('before {0:f} after', 42.123)
         self._test('before {0:F} after', 42.123)
 
-    def test_single_fp_auto(self):
+    def test_fp_auto(self):
         """Floating point format. Uses exponential format if exponent is
         greater than -4 or less than precision, decimal format otherwise.
         """
@@ -87,15 +154,102 @@ class ParserTest(unittest.TestCase):
         self._test('before {0:g} after', 42.123e-10)
         self._test('before {0:G} after', 42.123e-10)
 
-    def test_single_percent(self):
+    def test_percent(self):
         "Parse single percent"
         self._test('before {0:%} after', 42)
 
-    def test_single_unnamed(self):
+    def test_unnamed(self):
         self._test('before {:d} after', 42)
 
-    def test_single_named(self):
+    def test_named(self):
         self._test_dict('before {a:d} after', a=42)
+        self._test_dict('before {a:d} in between {b:d} after', a=42, b=23)
+
+    @unittest.expectedFailure
+    def test_attributes(self):
+        h = Dummy()
+        h.first = 'something'
+        h.seccond = 'else'
+
+        fmt = 'before {0.first} after'
+        text = fmt.format(h)       
+        obj = Parser(fmt)(text)
+        self.assertEqual(obj.first, h.first)
+
+        fmt = 'before {0.first} in between {0.second} after'
+        text = fmt.format(h)
+        obj = Parser(fmt)(text)
+        self.assertEqual(obj.first, h.first)
+        self.assertEqual(obj.second, h.second)
+
+        fmt = 'before {0.first} in between {1.second} after'
+        text = fmt.format(h)
+        obj1, obj2 = Parser(fmt)(text)
+        self.assertEqual(obj1.first, h.first)
+        self.assertEqual(obj2.second, h.second)        
+
+
+    def test_items(self):
+        h = dict()
+        h['first'] = 'something'
+        h['second'] = 'else'
+
+        fmt = 'before {0[first]} after'
+        text = fmt.format(h)       
+        obj = Parser(fmt)(text)
+        self.assertEqual(obj['first'], h['first'])
+
+        fmt = 'before {0[first]} in between {0[second]} after'
+        text = fmt.format(h)
+        obj = Parser(fmt)(text)
+        self.assertEqual(obj['first'], h['first'])
+        self.assertEqual(obj['second'], h['second'])
+
+        fmt = 'before {0[first]} in between {1[second]} after'
+        text = fmt.format(h, h)
+        obj1, obj2 = Parser(fmt)(text)
+        self.assertEqual(obj1['first'], h['first'])
+        self.assertEqual(obj2['second'], h['second'])        
+
+    def test_items_attributes(self):
+        h = Dummy()
+        h.first = {'second': 'something'}
+
+        fmt = 'before {0.first[second]} after'
+        text = fmt.format(h)       
+        obj = Parser(fmt)(text)
+        self.assertEqual(obj.first['second'], h.first['second'])
+
+        h = {'first': Dummy()}
+        h['first'].second = 'something'
+        fmt = 'before {0[first].second} after'
+        text = fmt.format(h)
+        obj = Parser(fmt)(text)
+        self.assertEqual(obj['first'].second, h['first'].second)
+     
+
+    def test_object_items(self):
+        h = dict()
+        h['aprop'] = 'middle'
+        h['aprop2'] = 'second'
+
+        fmt = 'before {0[aprop]} after'
+        text = fmt.format(h)       
+        obj = Parser(fmt)(text)
+        self.assertEqual(obj['aprop'], h['aprop'])
+
+        fmt = 'before {0[aprop]} in between {0[aprop2]} after'
+        text = fmt.format(h)
+        obj = Parser(fmt)(text)
+        self.assertEqual(obj['aprop'], h['aprop'])
+        self.assertEqual(obj['aprop2'], h['aprop2'])
+
+        fmt = 'before {0[aprop]} in between {1[aprop2]} after'
+        text = fmt.format(h, h)
+        obj1, obj2 = Parser(fmt)(text)
+        self.assertEqual(obj1['aprop'], h['aprop'])
+        self.assertEqual(obj2['aprop2'], h['aprop2'])      
+
 
     def test_many_numbered(self):
         "Parse many numbered"
@@ -109,19 +263,15 @@ class ParserTest(unittest.TestCase):
         "Parse many unnamed"
         self._test_many('before {:d} after {:d} end', 42, 43)
 
-    def test_single_ignore(self):
+    def test_ignore(self):
         "Parse single"
         parser = Parser('before {_:d} after')
-        self.assertEqual(parser('before 42 after'), ())
+        self.assertEqual(parser('before 42 after'), OrderedDict())
 
     def test_fail(self):
         "Parse fail"
         parser = Parser('before {:d} after')
         self.assertRaises(ValueError, parser, 'before bla after')
-
-    def test_just_string(self):
-        "Parse single string"
-        self._test('{0:s}', 'TEST')
 
 if __name__ == '__main__':
     unittest.main()
